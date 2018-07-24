3e256dc8d3640107e1e18bbcac9ac76283a357b7
Dylan Lange <dylan@paperkite.co.nz>
2018-07-25 08:46:35 +1200

    Start hooking in to lifecycle and trying out MVP

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -4,25 +4,56 @@ import { PokedexPage } from '../pokedex/pokedex';
 import { FavouritesPage } from '../favourites/favourites';
 import { AccountPage } from '../account/account';
 
-/**
- * Generated class for the MainPage tabs.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html'
 })
-export class MainPage {
+export class MainPage implements View {
+
+	presenter = MainPresenter(this);
 
   pokedexRoot = PokedexPage;
   favouritesRoot = FavouritesPage;
   accountRoot = AccountPage;
 
-
   constructor(public navCtrl: NavController) {}
 
+  ionViewDidLoad() {
+
+  }
+
+  ionViewWillEnter() {
+
+  }
+
+  ionViewDidEnter() {
+
+  }
+
+  ionViewWillLeave() {
+
+  }
+
+  ionViewWillUnload() {
+  	
+  }
+
+}
+
+
+class MainPresenter implements Presenter{
+
+	constructor(view: MainPage) {
+
+	}
+
+}
+
+interface View {
+
+}
+
+interface Presenter {
+	
 }
